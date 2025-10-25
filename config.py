025c1f348c272266556766a404f8843b1a1274b8
govarbot66-beep <govarbot66@gmail.com>
2025-10-25 22:30:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5665480584"))
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://teddugovardhan544_db_user:WVjIA96jQ31net0j@cluster0.kwkkleo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Advance file store")
+DB_NAME = os.environ.get("DATABASE_NAME", "Advance-file-store")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0")) 
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/All_animes_in_teluguu_vs")
